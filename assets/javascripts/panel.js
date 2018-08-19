c83389a23a59f7ff7d6f005f7d2adcf787b56aec
Julian <julianmklinger@gmail.com>
2018-08-19 18:23:43 +0200

    - fixed panel

--- assets/javascripts/panel.js
+++ assets/javascripts/panel.js
@@ -2,9 +2,15 @@
 $(function() {
   $('.panel-button').click(function () {
     var panelID = $(this).attr('data-panelid');
-    $('#' + panelID).show(200);
-    $('#' + panelID).siblings().hide();
-    $(this).addClass('btn-active');
-    $(this).parents().siblings().children().removeClass('btn-active');
+    if  ($(this).hasClass('btn-active')) {
+      $(this).removeClass('btn-active');
+      $('#' + panelID).hide(600);
+    }
+    else {
+      $('#' + panelID).show(600);
+      $('#' + panelID).siblings().hide();
+      $(this).addClass('btn-active');
+      $(this).parents().siblings().children().removeClass('btn-active');
+    }
   });
 })
